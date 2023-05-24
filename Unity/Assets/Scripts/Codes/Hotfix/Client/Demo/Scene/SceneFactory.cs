2977dc9b36c368197e52f5049a8af74acff0d825
uhmmm-xy <744165024@qq.com>
2023-05-24 17:17:08 +0800

    ClientHttpHelper

--- Unity/Assets/Scripts/Codes/Hotfix/Client/Demo/Scene/SceneFactory.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Client/Demo/Scene/SceneFactory.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Net.Sockets;
+using MongoDB.Bson;
 
 namespace ET.Client
 {
@@ -8,24 +10,35 @@ namespace ET.Client
         {
             await ETTask.CompletedTask;
             
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("channel_id", "1");
+            param.Add("type", "phone");
+            param.Add("phone", "15202302991");
+            param.Add("code", "123456");
+            
+            string ret = await HttpHelper.SendPost("http://192.168.50.243/api/login/login", param);
+
+            Dictionary<string,string> json = JsonHelper.FromJson<Dictionary<string, string>>(ret);
+            
+            Log.Info(json.ToJson());
+            Log.Info(ret);
             Scene clientScene = EntitySceneFactory.CreateScene(zone, SceneType.Client, name, ClientSceneManagerComponent.Instance);
             clientScene.AddComponent<CurrentScenesComponent>();
             clientScene.AddComponent<ObjectWait>();
             clientScene.AddComponent<PlayerComponent>();
-            
+
             EventSystem.Instance.Publish(clientScene, new EventType.AfterCreateClientScene());
             return clientScene;
         }
-        
+
         public static Scene CreateCurrentScene(long id, int zone, string name, CurrentScenesComponent currentScenesComponent)
         {
-            Scene currentScene = EntitySceneFactory.CreateScene(id, IdGenerater.Instance.GenerateInstanceId(), zone, SceneType.Current, name, currentScenesComponent);
+            Scene currentScene = EntitySceneFactory.CreateScene(id, IdGenerater.Instance.GenerateInstanceId(), zone, SceneType.Current, name,
+                currentScenesComponent);
             currentScenesComponent.Scene = currentScene;
-            
+
             EventSystem.Instance.Publish(currentScene, new EventType.AfterCreateCurrentScene());
             return currentScene;
         }
-        
-        
     }
 }
---
+++ Unity/Assets/Scripts/Codes/Hotfix/Client/Game/Helper/HttpHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using MongoDB.Bson;
+
+namespace ET.Client
+{
+    public static class HttpHelper
+    {
+        public static async ETTask<string> SendPost(string url, Dictionary<string, string> body)
+        {
+            string json = body.ToJson();
+            HttpContent data = new StringContent(json, Encoding.UTF8, "application/json");
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync(url, data);
+            string result = response.Content.ReadAsStringAsync().Result;
+            return result;
+        }
+
+        public static async ETTask<string> SendGet(string url, Dictionary<string, string> body)
+        {
+            string param = body.Aggregate("",
+                (current, item) => current + $"{item.Key}={item.Value}&");
+            using HttpClient client = new HttpClient();
+            StringBuilder urlSb = new StringBuilder(url);
+            HttpResponseMessage response = await client.GetAsync(urlSb.Append(param.TrimEnd('&')).ToString());
+            string result = response.Content.ReadAsStringAsync().Result;
+            return result;
+        }
+        
+    }
+}
